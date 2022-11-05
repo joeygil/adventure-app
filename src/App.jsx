f2412d099507c0b014bf516f5e6e67ac049059c0
joeygil <105308164+joeygil@users.noreply.github.com>
2022-11-05 09:00:39 +0000

    json error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const handleClick = async () => {
     setIsLoading(true);
-    const response = await fetch("adventures.json");
+    const response = await fetch("src/assets/adventures.json");
     if (!response.ok) {
       throw new Error(`Error! Status ${response.status}`);
     }
