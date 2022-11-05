618e994901eb906dc60d87b87741baf502b1c9c1
joeygil <105308164+joeygil@users.noreply.github.com>
2022-11-05 08:36:35 +0000

    congif update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+
+  assetsInclude: ["/src/assets/images/off-road.png"],
 });
